45e02b6aa39bde3d3081c8ec7cc962f8380c1052
kisakov <isakov90@gmail.com>
2018-08-11 17:48:03 -0700

    add colors

--- lib/choose.rb
+++ lib/choose.rb
@@ -1,13 +1,14 @@
 require 'choose/version'
 require 'choose/file_processor'
+require 'colorize'
 
 module Choose
   def self.run!
     if ARGV.size == 2
       FileProcessor.move!(ARGV[0], ARGV[1])
     else
-      puts "Invalid arguments! Please provide two folders:"
-      puts "choose original_folder choosen_folder"
+      puts "Invalid arguments! Please provide two folders:".colorize(:red)
+      puts "choose original_folder choosen_folder".colorize(:green)
     end
   end
 end
--- lib/choose/file_processor.rb
+++ lib/choose/file_processor.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'colorize'
 
 module FileProcessor
   def self.move!(original_dir, choosen_dir)
@@ -6,23 +7,25 @@ module FileProcessor
     choosen_files = Dir["#{choosen_dir}/*"].map { |file| filename(file) }
     extra_files = original_files - choosen_files
 
-    dst_dir = "extra"
-    FileUtils.mkdir_p(dst_dir)
-
     Dir["#{original_dir}/*"].select do |original_file|
       extra_files.include?(File.basename(original_file, '.*'))
     end.tap do |files|
       if files.any?
+        dst_dir = "extra"
+        FileUtils.mkdir_p(dst_dir)
+
         puts "Moving files:"
       else
-        puts "Nothing to move!"
+        puts "Nothing to move!".colorize(:red)
       end
     end.each do |file|
       dst = "#{dst_dir}/#{File.basename(file)}"
-      puts "  #{file} -> #{Dir.pwd}/#{dst}"
+      puts "  #{file} -> #{Dir.pwd}/#{dst}".colorize(:light_blue)
       FileUtils.mv(file, dst)
     end.tap do |files|
-      puts "#{files.size} file(s) mooved to #{Dir.pwd}/extra folder" if files.any?
+      if files.any?
+        puts "#{files.size} file(s) mooved to #{Dir.pwd}/extra folder".colorize(:green)
+      end
     end
   end
 
